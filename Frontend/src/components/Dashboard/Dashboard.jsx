b261dd41c3c1e280200da55208c7a1b83fb45b48
Aaditya Singhal <aadityasinghal1978@gmail.com>
2023-04-02 13:35:31 +0530

    added dashboard with navbar

--- Frontend/src/components/Dashboard/Dashboard.jsx
+++ Frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
 import { HelmetProvider, Helmet } from "react-helmet-async";
+import {
+    Box,
+    Toolbar
+} from "@mui/material"
+import { HashLoader } from "react-spinners";
+import Navbar from "../Navbar/Navbar";
 import "./Dashboard.scss";
 
 export default function Dashboard() {
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
     return (
         <HelmetProvider>
 
@@ -11,10 +28,24 @@ export default function Dashboard() {
                 <meta name="description" content="Welcome to your dashboard!" />
             </Helmet>
 
-            {/* dashboard */}
-            <div className="dashboard">
+            { loading ? (
+                <div className="h-loader">
+                    <HashLoader color="#74DBEF" loading={loading} size={200} />
+                </div>
+            ) : (
 
-            </div>
+            <>
+                {/* Navbar */}
+                <Navbar />
+
+                {/* dashboard */}
+                <Box className="dashboard" sx={{ p: 3 }}>
+                    <Toolbar />
+                    heelo
+                </Box>
+            </>
+            )}
+            
         </HelmetProvider>
     );
 }
