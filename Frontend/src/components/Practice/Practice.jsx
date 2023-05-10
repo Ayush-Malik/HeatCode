c43c91797528dde45d6401f755e213038a6e9d11
unknown <aadityasinghal1978@gmail.com>
2023-05-10 15:08:05 +0530

    add

--- Frontend/src/components/Practice/Practice.jsx
+++ Frontend/src/components/Practice/Practice.jsx
@@ -2,7 +2,23 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { useSelector, useDispatch } from "react-redux";
-import { Box, Toolbar } from "@mui/material"
+import { 
+    Box,
+    Toolbar,
+    Grid,
+    Button,
+    FormGroup,
+    FormControlLabel,
+    Checkbox,
+    TextField,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TablePagination
+} from "@mui/material"
 import { apiCall } from "../../utils/api";
 import { getStorage } from "../../utils/localStore";
 import { setUser } from "../../redux/slices/userSlice";
@@ -21,6 +37,21 @@ export default function Practice() {
     const [loading, setLoading] = useState(true);
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
+    const checkColor = {color: '#74DBEF','&.Mui-checked': {color: '#74DBEF'}};
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(20);
+    const [visibleRows, setVisibleRows] = useState(20);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+        setVisibleRows(newPage * rowsPerPage + rowsPerPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+        setVisibleRows(+event.target.value);
+    };
 
     useEffect(() => {
         if(user === null){
@@ -64,7 +95,7 @@ export default function Practice() {
                 <Navbar setLoading={setLoading} />
 
                 {/* practice */}
-                <Box className="practice" sx={{ p: 3 }}>
+                <Box className="practice" sx={{ p: 6 }}>
                     <Toolbar />
                     
                     {/* Crousel */}
@@ -123,6 +154,118 @@ export default function Practice() {
                                 </div>
                             </div>
                         </Carousel>
+                    </div>
+
+                    {/* problems */}
+                    <div className="practice_problems">
+                        <h2>Practice Problems</h2>
+
+                        <Grid container spacing={2} className="practice_problems_container">
+                            <Grid md={3} item className="practice_problems_left">
+                                <div className="header">
+                                    <h2>Filters</h2>
+                                    <Button className="cls-btn">
+                                        Clear All
+                                    </Button>
+                                </div>
+
+                                <div className="filters">
+                                    <div className="filter">
+                                        <h3>Difficulty</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+
+                                    <div className="filter">
+                                        <h3>Topics</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+                                    <div className="filter">
+                                        <h3>Topics</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+                                    <div className="filter">
+                                        <h3>Topics</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+                                    <div className="filter">
+                                        <h3>Topics</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+                                    <div className="filter">
+                                        <h3>Topics</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+                                    <div className="filter">
+                                        <h3>Topics</h3>
+                                        <FormGroup className="options">
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Label" />
+                                            <FormControlLabel control={<Checkbox sx={checkColor} />} label="Disabled" />
+                                        </FormGroup>
+                                    </div>
+                                </div>
+                            </Grid>
+
+                            <Grid md={9} item className="practice_problems_right">
+                                <div className="practice_search">
+                                    <TextField className="search" label="Search Questions" variant="outlined" />
+                                </div>
+
+                                <div className="practice_problems_list">
+                                    <TableContainer>
+                                        <Table className="table">
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>Problem</TableCell>
+                                                    <TableCell>Difficulty</TableCell>
+                                                    <TableCell>Topics</TableCell>
+                                                    <TableCell>Accuracy</TableCell>
+                                                    <TableCell>Status</TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                { Array(visibleRows).fill().map((_, index) => (
+                                                    <TableRow>
+                                                        <TableCell>1. Solve Me First</TableCell>
+                                                        <TableCell>Easy</TableCell>
+                                                        <TableCell>Warmup</TableCell>
+                                                        <TableCell>100%</TableCell>
+                                                        <TableCell>Not Attempted</TableCell>
+                                                    </TableRow>
+                                                )) }
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                    <TablePagination
+                                        className="table_pagination"
+                                        rowsPerPageOptions={[10, 20, 50, 100]}
+                                        component="div"
+                                        count={100}
+                                        rowsPerPage={rowsPerPage}
+                                        page={page}
+                                        onPageChange={handleChangePage}
+                                        onRowsPerPageChange={handleChangeRowsPerPage}
+                                    />
+                                </div>
+                            </Grid>
+                        </Grid>
                     </div>
 
                 </Box>
--- Frontend/src/components/Profile/Profile.jsx
+++ Frontend/src/components/Profile/Profile.jsx
@@ -5,17 +5,59 @@ import { useSelector, useDispatch } from "react-redux";
 import { apiCall } from "../../utils/api";
 import { getStorage } from "../../utils/localStore";
 import { setUser } from "../../redux/slices/userSlice";
-import { Box, Toolbar } from "@mui/material"
+import { Box, Toolbar, Grid, FormControl, Select, MenuItem } from "@mui/material"
 import { HashLoader } from "react-spinners";
+import { FaUserAlt, FaRegEdit } from "react-icons/fa";
+import { BsGithub, BsLinkedin, BsGlobe} from "react-icons/bs";
+import CalendarHeatmap from 'react-calendar-heatmap';
+import ReactTooltip from 'react-tooltip';
+import 'react-calendar-heatmap/dist/styles.css';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    RadialLinearScale,
+    ArcElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import { Doughnut, Line } from "react-chartjs-2"
 import Navbar from "../Navbar/Navbar";
 import "./Profile.scss";
 
 export default function Profile() {
 
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        BarElement,
+        RadialLinearScale,
+        ArcElement,
+        Title,
+        Tooltip,
+        Legend
+    );    
     const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
+
+    const getTooltipDataAttrs = (value) => {
+        // Temporary hack around null value.date issue
+        if (!value.date) {
+          return null;
+        }
+        // Configuration for react-tooltip
+        return {
+          'data-tip': `Submissions on ${value.date}: ${value.count || 0}`,
+        };
+    };
 
     useEffect(() => {
         if(user === null){
@@ -59,8 +101,176 @@ export default function Profile() {
                 <Navbar setLoading={setLoading} />
 
                 {/* profile */}
-                <Box className="profile" sx={{ p: 3 }}>
+                <Box className="profile_s" sx={{ p: 6 }}>
                     <Toolbar />
+
+                    {/* profile */}
+                    <Grid container spacing={2} className="profile">
+                        <Grid md={2.7} item className="profile_info">
+                            <FaRegEdit className="edit_icon" />
+                            <FaUserAlt className="user_image" />
+                            <h2>Aaditya</h2>
+                            <span className="user_email">
+                                aaditya@gmail.com
+                            </span>
+                            <span className="user_bio">
+                                -
+                            </span>
+                            <div className="user_links">
+                                <a href="google.com" target="_blank" rel="noreferrer">
+                                    <BsGithub className="user_link" />
+                                </a>
+                                <a href="google.com" target="_blank" rel="noreferrer">
+                                    <BsLinkedin className="user_link" />
+                                </a>
+                                <a href="google.com" target="_blank" rel="noreferrer">
+                                    <BsGlobe className="user_link" />
+                                </a>
+                            </div>
+                            <hr />
+                            <div className="user_about">
+                                <h3>About</h3>
+                                <p className="user_about_item">
+                                    JMIT
+                                </p>
+                                <p className="user_about_item">
+                                    JMIT
+                                </p>
+                            </div>
+                        </Grid>
+                        <Grid md={9.3} item className="profile_stats">
+                            <div className="profile_heatmap">
+                                <h2>
+                                    Submission Heatmap
+                                </h2>
+                                <CalendarHeatmap
+                                    startDate={new Date('2022-01-01')}
+                                    endDate={new Date('2023-04-01')}
+                                    values={[
+                                        { date: '2022-11-01', count: 121 },
+                                        { date: '2022-01-22', count: 323 },
+                                        { date: '2022-01-30', count: 3 },
+                                    ]}
+                                    classForValue={(value) => {
+                                        if (!value) {
+                                        return 'color-empty';
+                                        }
+                                        else if (value.count > 0 && value.count <= 100) {
+                                            return 'color-scale-1';
+                                        }
+                                        else if (value.count > 100 && value.count <= 200) {
+                                            return 'color-scale-2';
+                                        }
+                                        else if (value.count > 200 && value.count <= 300) {
+                                            return 'color-scale-3';
+                                        }
+                                        else if (value.count > 300 && value.count <= 400) {
+                                            return 'color-scale-4';
+                                        }
+                                    }}
+                                    tooltipDataAttrs={(value) => getTooltipDataAttrs(value)}
+                                />
+                                <ReactTooltip />
+                            </div>
+                            <div className="stats">
+                                <div className="submissions">
+                                    <h2>
+                                        Submissions
+                                    </h2>
+                                    <Doughnut
+                                        data={{
+                                            labels: ["Easy", "Medium", "Hard"],
+                                            datasets: [
+                                                {
+                                                    data: [10, 20, 40],
+                                                    backgroundColor: [
+                                                        "#28e461",
+                                                        "#c8e428",
+                                                        "#e43428",
+                                                    ],
+                                                    hoverOffset: 4,
+                                                },
+                                            ],
+                                        }}
+                                        options={{
+                                            responsive: true,
+                                            plugins: {
+                                                legend: {
+                                                    display: true,
+                                                },
+                                            },
+                                        }}
+                                    />
+                                </div>
+                                <div className="monthly">
+                                    <h2>
+                                        Monthly Submissions
+                                    </h2>
+                                    <FormControl className="select">
+                                        <Select
+                                            value={10}
+                                        >
+                                            <MenuItem value={10}>October</MenuItem>
+                                            <MenuItem value={20}>November</MenuItem>
+                                            <MenuItem value={30}>December</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <Line
+                                        data={{
+                                            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+                                            datasets: [
+                                                {
+                                                    label: "Monthly Submissions",
+                                                    data: [10, 20, 40, 30, 20, 10, 20, 30, 40, 50, 60, 70, 80, 90, 0, 10, 20, 30, 40, 50, 60, 74, 18, 19, 20, 10, 20, 0, 20, 20],
+                                                    fill: false,
+                                                    backgroundColor: "#74DBEF",
+                                                    borderColor: "#74DBEF",
+                                                },
+                                            ],
+                                        }}
+                                        options={{
+                                            responsive: true,
+                                            plugins: {
+                                                legend: {
+                                                    display: true,
+                                                },
+                                            },
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="contest">
+                                <h2>
+                                    Contests Activity
+                                </h2>
+                                <div className="contest_list">
+                                    <div className="contest_item">
+                                        <div className="contest_item_info">
+                                            Starter 73
+                                        </div>
+                                        <div className="contest_item_info">
+                                            10/10/2021 - 10/10/2021
+                                        </div>
+                                        <div className="contest_item_info">
+                                            100 / 200
+                                        </div>
+                                    </div>
+                                    <div className="contest_item">
+                                        <div className="contest_item_info">
+                                            Starter 73
+                                        </div>
+                                        <div className="contest_item_info">
+                                            10/10/2021 - 10/10/2021
+                                        </div>
+                                        <div className="contest_item_info">
+                                            100 / 200
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Grid>
+                    </Grid>
+
                 </Box>
             </>
             )}
